d37f2dbeaf016833c15fbd3ac772f751fefb4eba
Xingxin Chen <xingxinc@andrew.cmu.edu>
2018-05-26 18:28:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay (musicians,instruments) {
 var array = []
-for (let i=0; l= musicians.length; i < l; i++){
+for (let i= 0, l= musicians.length; i < l; i++){
 array.push( `musicians[i] plays instruments[i]`)
 }
   return array
